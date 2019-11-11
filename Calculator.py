240ab3fce6bcf1fee5149d73e44f055c52783a91
BarbarianJan <januszjerzymajewski@gmail.com>
2019-11-11 21:28:03 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -7,12 +7,20 @@ run = True
 def performMath():
     global run
     global previous
-    equation = input("Enter equation:")
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+        equation = input(str(previous))
+
     if equation == 'quit':
+        pring("Goodbye hooman")
         run = False
     else:
-        equation=re.sub('[a-z-A-Z,.()" "]','', equation)
-        previous = eval(equation)
-        print("You typed", previous)
+        equation=re.sub('[a-zA-Z,.()" "]','', equation)
+        if previous ==0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous)+equation)
 while run:
     performMath()
