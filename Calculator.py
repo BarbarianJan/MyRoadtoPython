46e40c9adb00613b40db78b53720fce5384f1fc5
BarbarianJan <januszjerzymajewski@gmail.com>
2019-11-11 21:14:19 +0100

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+import re
+print("Our Magical Calculator")
+print("Type 'quit' to exit\n")
+previous = 0
+run = True
+
+def performMath():
+    global run
+    global previous
+    equation = input("Enter equation:")
+    if equation == 'quit':
+        run = False
+    else:
+        previous = eval(equation)
+        print("You typed", previous)
+while run:
+    performMath()
